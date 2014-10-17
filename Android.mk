cd9c74fd961cbe7d3c1c69769ce392314afc520b
Jyoti Wadhwani <jyotiw@codeaurora.org>
2014-10-17 11:57:10 -0700

    Keymaster: Support for 64bit userspace and 32bit TZ
    HW keymaster is not supported for 64bit userspace. This patch
    updates variable types to support the architecture.
    It also makes change in Android.mk file to support all qcom
    platforms.
    
    Change-Id: I25633d707b425388bcc116196c0e0022c5c4f9a5
    
    keymaster: Fix build for devices that don't define BOARD_USES_QCOM_HARDWARE
    
    Change-Id: Ia80c28920ac8d8fd0ce3ac703977dc8a5a0651ce
    
    keymaster: Allow 8952 to build
    
    Change-Id: I9c2d5a0e4fc584f7909e0733827d5499191d82a3
    
    keymaster: add msm8909 into support list
    
    Issue-Id: FEIJ-8
    
    Change-Id: I1252ec5f1171ca94635e6f58d835a588948be771

--- Android.mk
+++ Android.mk
@@ -7,7 +7,7 @@ ifneq ($(filter msm8960 msm8084,$(TARGET_BOARD_PLATFORM)),)
 
 keymaster-def := -fvisibility=hidden -Wall
 ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
-ifneq ($(filter msm8960 msm8226 msm8974 msm8610 msm8084 apq8084,$(TARGET_BOARD_PLATFORM)),)
+ifneq ($(filter msm8960 msm8226 msm8974 msm8610 msm8084 apq8084 msm8909 msm8916 msm8952 msm8992 msm8994,$(TARGET_BOARD_PLATFORM)),)
 keymaster-def += -D_ION_HEAP_MASK_COMPATIBILITY_WA
 endif
 else
@@ -23,7 +23,7 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE := keystore.$(TARGET_BOARD_PLATFORM)
 
-LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
+LOCAL_MODULE_RELATIVE_PATH := hw
 
 LOCAL_SRC_FILES := keymaster_qcom.cpp
 
